46e62f759fb5aaa54aedfb269f6929aa47408025
Allan Torres <allaneduardot@gmail.com>
2022-03-23 18:22:08 -0400

    New featurs without messages

--- api/src/routes/users.js
+++ api/src/routes/users.js
@@ -6,40 +6,13 @@ const users = require('../controllers/user')
 router.post('/signup', users.createUser)
 
 
-//logearse
-router.post('/login', async(req, res) =>{
-    try {
-        
-    } catch (error) {
-        
-    }
-});
 
-// //todos los usuarios
-// router.get('/', users.findAllUser)
-  
-
-// //obtener usuario
-// router.get('/:id', async(req, res) =>{
-//     try {
-        
-//     } catch (error) {
-        
-//     }
-// });
-
-// actualizar usuario
-router.put('/:id', users.updateUser)
+ 
     
 // encontrar usuario
-router.get('/', users.findUser)
+router.post('/', users.findUser)
 
-// eliminar usuario
-router.delete('/:id', users.deleteUser)
-
-
-// enviar mail de actualizacion de contraseña
-router.post('/resetpass', users.requestPasswordReset)
+ 
 
  
 
